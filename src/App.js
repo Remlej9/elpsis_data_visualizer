46beeb65aa66a93e373e84e106ebb9a9158c2739
Remlej9 <Remlej9@gmail.com>
2023-11-17 19:03:25 +0100

    Does not compile

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from 'react';
+import { Canvas, useFrame } from 'react-three-fiber';
+import { Euler } from 'three';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const RotatingObject = ({ rotationValues }) => {
+    const meshRef = useRef();
+
+    useFrame(() => {
+        const [x, y, z] = rotationValues;
+        meshRef.current.rotation.set(x, y, z);
+    });
+
+    return (
+        <mesh ref={meshRef}>
+            <boxBufferGeometry args={[1, 1, 1]} />
+            <meshStandardMaterial color="orange" />
+        </mesh>
+    );
+};
+
+const App = () => {
+    // Example rotation data over time
+    const rotationData = [
+        [0, 0, 0],
+        [Math.PI / 4, Math.PI / 4, Math.PI / 4],
+        [Math.PI / 2, Math.PI / 2, Math.PI / 2],
+        [Math.PI, Math.PI, Math.PI],
+    ];
+
+    return (
+        <Canvas>
+            <ambientLight />
+            <pointLight position={[10, 10, 10]} />
+            <RotatingObject rotationValues={rotationData[0]} />
+        </Canvas>
+    );
+};
 
 export default App;
