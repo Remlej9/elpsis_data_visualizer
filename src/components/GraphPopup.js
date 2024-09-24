1037fd847e09b2511d48595c173833e33d021d2f
Remlej9 <johan@sjolunds.se>
2024-09-24 19:18:46 +0200

    feat: altitude graph updated

--- src/components/GraphPopup.js
+++ src/components/GraphPopup.js
@@ -1,33 +1,28 @@
 import React from 'react';
-import Draggable from 'react-draggable'; // Import the Draggable component
+import Draggable from 'react-draggable';
 import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
 import accelerationdata from "../data/accelerationdata";
+import altitudedata from "../data/altitudedata";
 import '../css/GraphPopup.css';
 
-const data = accelerationdata.map((value, index) => ({
-    time: index / 10,
-    acceleration: value.value
-}));
-
 const GraphPopup = ({ type, frame }) => {
-    const slice = data.slice(0, frame + 1);
+    // Use either acceleration data or altitude data depending on the type
+    const data = type === 'acceleration' ? accelerationdata : altitudedata;
+
+    const slice = data.slice(0, frame + 1).map((value, index) => ({
+        time: index / 10,
+        value
+    }));
 
     return (
         <Draggable>
-            {/* Wrap your content with Draggable component */}
             <div className="graph-popup">
-                {/*
-                    <div className="close-button" onClick={onClose}>
-                        Close
-                    </div>
-                */}
-
                 <div className="graph-title">
                     {type}
                 </div>
 
                 <LineChart width={250} height={250} data={slice}>
-                    <Line type="monotone" dataKey={type} stroke="#8884d8" strokeWidth={3} />
+                    <Line type="monotone" dataKey="value" stroke="#8884d8" strokeWidth={3} />
                     <CartesianGrid stroke="#ccc" />
                     <XAxis dataKey="time" domain={[0, slice.length]}/>
                     <YAxis domain={[0, 1]}/>
