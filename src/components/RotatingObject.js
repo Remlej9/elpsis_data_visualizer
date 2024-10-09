8cd9fbee3b264b59b2e69aab7e3d9806b42e00c2
Remlej9 <johan@sjolunds.se>
2024-10-09 11:46:29 +0200

    fix: small fixes to rotating object

--- src/components/RotatingObject.js
+++ src/components/RotatingObject.js
@@ -1,26 +1,18 @@
 import React, {useRef} from 'react';
 import {useFrame} from 'react-three-fiber';
-// import {FBXLoader} from "three/examples/jsm/loaders/FBXLoader";
-// import {OBJLoader} from "three/examples/jsm/loaders/OBJLoader";
 
 const RotatingObject = ({ rotationValues }) => {
     const groupRef = useRef();
 
-    /*const fbx = useLoader(FBXLoader, '../object/source/FBX 2013/day_3_Punchrulle.fbx');
-    const obj = useLoader(OBJLoader, '../object/source/OBJ 2013/day_3_Punchrulle.obj');
-    const colorMap = useLoader(OBJLoader, '../object/source/OBJ 2013/day_3_Punchrulle.mtl');*/
-
     useFrame(() => {
         const [x, y, z] = rotationValues;
-        groupRef.current.rotation.set(y, x, z);
+        if (groupRef.current) {
+            groupRef.current.rotation.set(y, x, z);
+        }
     });
 
     return (
         <group ref={groupRef}>
-            {/*obj && <primitive object={obj}
-            map={colorMap}
-            />*/}
-            {/*fbx && <primitive object={fbx} />*/}
             <mesh position={[0, 2, 0]}>
                 <coneGeometry args={[0.9, 2, 32]} attach="geometry" />
                 <meshStandardMaterial color="#349ecb" />
